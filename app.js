140f04fd4e3eae12cb685f4269ba7911c7e7ee89
Clint <clintmad21@hotmail.com>
2017-10-15 17:08:00 -0600

    More

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var urlencode = bodyParser.urlencoded({ extended: false });
 
 app.use(express.static('public'));
 
-app.get('/subscriptions', function(req, res) {
-    var subscriptions = [];
-    res.json(subscriptions);
+var redis = require('redis');
+var client = redis.createClient();
+
+client.select((process.env.NODE_ENV || 'development').length);
+
+app.get('/subscriptions', function (req, res) {
+    client.hkeys('subscriptions', function (err, names) {
+        if(err) throw err
+        res.json(names);
+    });
+});
+
+app.post('/subscriptions', urlencode, function (req, res) {
+    var newSubscription = req.body;
+    client.hset('subscriptions', newSubscription.name, newSubscription.description, function(err) {
+        if(err) throw err
+        res.status(201).json(newSubscription.name);
+    });
+    
 });
 
 
--- test.js
+++ test.js
@@ -1,6 +1,11 @@
 var request = require('supertest');
 var app = require('./app');
 
+var redis = require('redis');
+var client = redis.createClient();
+client.select('test'.length);
+client.flushdb();
+
 describe('Requests to the root path', function () {
 
     it('Returns a 200 status code', function (done) {
@@ -43,5 +48,27 @@ describe('Listing subscriptions on /subscriptions', function () {
         request(app)
             .get('/subscriptions')
             .expect(JSON.stringify([]), done);
+    });
+});
+
+describe('Creating new subscriptions', function() {
+
+    before(function() {
+
+    });
+
+    it('Returns a 201 status code', function(done) {
+
+        request(app)
+            .post('/subscriptions')
+            .send('name&description&date')
+            .expect(201, done);
+    });
+    it('Return the subscription name', function(done) {
+
+        request(app)
+            .post('/subscriptions')
+            .send('name=Netflix&description=Netflix&date=02/02/2018')
+            .expect(/netflix/i, done);
     });
 });
